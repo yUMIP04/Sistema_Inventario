0378fea6249c11e346716aa2a30a35bac73127e2
VictoriaPerez <yumip5719@gmail.com>
2026-03-01 21:20:27 -0600

    Registro con exito

--- app.py
+++ app.py
@@ -1,11 +1,27 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, request, flash, session, url_for
+from werkzeug.security import generate_password_hash, check_password_hash
+from database import create_DB, create_Table, insert_user
+
 
 app = Flask(__name__)
 
+app.secret_key = 'SISTEMAINVENTARIOPARAPORTAFOLIO'
 
-@app.route('/')
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    
+    create_DB()
+    if request.method == 'POST':
+        
+        nombre_usuario = request.form['name']
+        contraseña = request.form['password']         
+        
+        contraseña_hasheada = generate_password_hash(contraseña)
+        create_Table()
+        insert_user(nombre_usuario, contraseña_hasheada)
+        
+        flash('Registro con exito', 'success')
+        
     return render_template("index.html")
 
 
--- database.py
+++ database.py
@@ -1,13 +1,12 @@
 import sqlite3
 from flask import Flask,g
 
-
+#🌟CREACION DE LA DB
 def create_DB():
-    
-    if 'db' not in g:
-        g.db = sqlite3.connect('Inventario.db')
-    return g.db   
-        
+    return  sqlite3.connect('Inventario.db')
+     
+
+#🌟CREACION DE LA TABLA DE USUARIOS
 def create_Table():
     
     conexion = create_DB()
@@ -15,11 +14,26 @@ def create_Table():
     
     cursor.execute('''
                    CREATE TABLE IF NOT EXISTS Usuario(
-                       id_usuario INT PRIMARY KEY AUTOINCREMENT,
-                       nombre_usuario VARCHAR(10),
-                       contraseña VARCHAR(8)
+                       id_usuario INTEGER PRIMARY KEY AUTOINCREMENT,
+                       nombre_usuario VARCHAR(10) UNIQUE,
+                       contraseña VARCHAR(8) UNIQUE
                    )
                    ''')
+    
+    conexion.commit()
+    conexion.close()
+    
+#🌟INSERTAR UN NUEVO USUARIO
+
+def insert_user(nombre_usuario, contraseña):
+    
+    conexion = create_DB()
+    cursor = conexion.cursor()
+    
+    
+    cursor.execute('''
+                   INSERT INTO Usuario(nombre_usuario, contraseña) VALUES (?,?) 
+                   ''', (nombre_usuario, contraseña))
     
     conexion.commit()
     conexion.close()
